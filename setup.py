f30e20e31c1788a853fa9e747631faacc9bfc4b4
Hans Bischoff <51087264+HasiSchoff@users.noreply.github.com>
2026-02-12 10:19:52 +0100

    Remove unsupported package pkg_resources

--- setup.py
+++ setup.py
@@ -2,14 +2,13 @@
 
 from pathlib import Path
 
-import pkg_resources as pkg
 from setuptools import find_packages, setup
 
 # Settings
 FILE = Path(__file__).resolve()
 PARENT = FILE.parent  # root directory
 README = (PARENT / "README.md").read_text(encoding="utf-8")
-REQUIREMENTS = [f"{x.name}{x.specifier}" for x in pkg.parse_requirements((PARENT / "requirements.txt").read_text())]
+REQUIREMENTS = [line.strip() for line in (PARENT / "requirements.txt").read_text().splitlines() if line.strip() and not line.startswith("#")]
 
 
 exec(open("mivolo/version.py").read())
